f52f26dabc6ccc4e3829645796414376c2bd6e94
缐英达 <xianyingda@guazi.com>
2018-12-31 01:20:35 +0800

    测试完成通过starter配置dubbo双协议

--- layer2-provider/src/main/java/com/guazi/test/layer2/api/impl/Layer2ApiImpl.java
+++ layer2-provider/src/main/java/com/guazi/test/layer2/api/impl/Layer2ApiImpl.java
@@ -8,16 +8,27 @@ import com.guazi.test.layer2.vo.Layer2Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Using IntelliJ IDEA.
  *
  * @author XIANYINGDA at 2018-12-29 13:59
  */
+@Path("/layer2")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
 @Component
 @Slf4j
 @Service
 public class Layer2ApiImpl implements Layer2Api {
 
+    @Path("/response")
+    @GET
     @Override
     public Layer2Response getLayer2Response(Layer2RequestVo requestVo) {
         log.info("传入参数：{}", JSON.toJSONString(requestVo));
