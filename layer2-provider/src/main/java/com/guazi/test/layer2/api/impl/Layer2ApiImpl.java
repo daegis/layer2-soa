f47f680dd4b8456d75c81a3bbdf64e5c1dfd2df2
缐英达 <xianyingda@guazi.com>
2018-12-31 00:25:09 +0800

    增加参数日志方便调试

--- layer2-provider/src/main/java/com/guazi/test/layer2/api/impl/Layer2ApiImpl.java
+++ layer2-provider/src/main/java/com/guazi/test/layer2/api/impl/Layer2ApiImpl.java
@@ -1,6 +1,7 @@
 package com.guazi.test.layer2.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.guazi.test.layer2.api.Layer2Api;
 import com.guazi.test.layer2.vo.Layer2RequestVo;
 import com.guazi.test.layer2.vo.Layer2Response;
@@ -19,6 +20,7 @@ public class Layer2ApiImpl implements Layer2Api {
 
     @Override
     public Layer2Response getLayer2Response(Layer2RequestVo requestVo) {
+        log.info("传入参数：{}", JSON.toJSONString(requestVo));
         Layer2Response response = new Layer2Response();
         if (requestVo.getAge() == 18) {
             throw new RuntimeException("第二层模拟异常");
